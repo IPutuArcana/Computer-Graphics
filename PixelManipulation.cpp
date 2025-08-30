7ef5fa863f71bd42c6c88fa79100d63a1ebd5240
IPutuArcana <aarrcane@gmail.com>
2025-08-30 08:17:26 -0400

    Add initial project files for C++ graphics application
    - Create launch configuration for debugging with g++
    - Define build tasks for g++ and g++-14
    - Implement basic window creation using X11 in PixelManipulation.cpp

---
+++ PixelManipulation.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<X11/Xlib.h>
+
+using namespace std;
+
+int main(){
+    Display *display;
+    Window window;
+    XEvent event;
+
+    display = XOpenDisplay(NULL);
+    if(display == NULL){
+        cerr<<"display nya KOSONG BROOOO"<<endl;
+        return 1;
+    }
+
+    int screen = DefaultScreen(display);
+    window = XCreateSimpleWindow(display, RootWindow(display, screen), 10, 10, 200, 200, 1, BlackPixel(display, screen), WhitePixel(display, screen));
+    
+    Atom delWindow = XInternAtom(display, "WM_DELETE_WINDOW", 0);
+    XSetWMProtocols(display, window, &delWindow, 1);
+
+    XMapWindow(display, window);
+    
+
+    cout<<"KELAS JALAN BROOO"<< endl;
+
+    XCloseDisplay(display);
+
+    return 0;
+}
